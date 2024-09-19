5d5a8b861095ce7fe6819cfb6aa938e343f986e6
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2024-09-19 17:31:08 +0530

    adding codegen

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,45 +1,40 @@
-import { gql } from 'urql';
 import client from './utils/client';
 import { Suspense } from 'react';
+import { GetFriends, GetFriendsQuery, GetFriendsQueryVariables } from '../../generated/graphql';
 
-const QUERY = gql`
-  query {
-    friend {
-      name
-    }
-  }
-`;
+interface Props {
+  friends: GetFriendsQuery["friend"]
+}
 
 async function fetchFriends() {
   try {
-    const result = await client.query(QUERY).toPromise();
-    return result.data.friend;
+    const result = await client.query<GetFriendsQuery, GetFriendsQueryVariables>(GetFriends, {}).toPromise();
+    return result.data?.friend || [];
   } catch (err: any) {
     throw new Error(err.message);
   }
 }
 
+export default async function Home() {
+  const friends = await fetchFriends();
 
-export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-8xl font-bold text-blue-600 mb-8">Hello World from FOOD-APP</h1>
       <Suspense fallback={<p>Loading...</p>}>
-        <FriendsList />
+        <FriendsList friends={friends} />
       </Suspense>
     </div>
   );
 }
 
-async function FriendsList() {
-  const friends = await fetchFriends();
-
+function FriendsList({ friends }: Props) {
   return (
     <ul className="space-y-4">
-      {friends.map((friend: any) => (
+      {friends.map((friend) => (
         <li
           key={friend.name}
-          className="px-12 py-9 bg-slate-900  rounded-lg shadow-md hover:bg-gold-200 transition text-4xl"
+          className="px-12 py-9 bg-slate-900 rounded-lg shadow-md hover:bg-gold-200 transition text-4xl"
         >
           <p className='text-slate-50'> {friend.name}</p>
         </li>
